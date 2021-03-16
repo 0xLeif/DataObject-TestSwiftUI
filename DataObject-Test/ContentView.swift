a9c02a0a03e1e87dee1b19daf0988bd53957eef9
Zach Eriksen <zmeriksen@icloud.com>
2021-03-15 21:38:19 -0500

    Initial Commit

--- DataObject-Test/ContentView.swift
+++ DataObject-Test/ContentView.swift
@@ -6,11 +6,42 @@
 //
 
 import SwiftUI
+import DataObject
 
 struct ContentView: View {
+    @State private var currentView: AnyView = AnyView(ProgressView())
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        currentView
+            .onAppear {
+                "https://avatars.githubusercontent.com/u/8268288?s=460&u=f3ef15376e7a613d4f739fa42624a07c03c0370e&v=4"
+                    .url?
+                    .func { (url) in
+                        DispatchQueue.global().async {
+                            url.get { (dataObject) in
+                                dataObject.sout()
+                                
+                                dataObject.data.value(as: Data.self)
+                                    .map { UIImage(data: $0) }?
+                                    .replace(nilWith: UIImage())
+                                    .func { image in
+                                        DispatchQueue.main.async {
+                                            currentView = AnyView(
+                                                Image(uiImage: image)
+                                                    .padding()
+                                            )
+                                        }
+                                    }
+                                
+                            }
+                        }
+                        
+                        currentView = AnyView(
+                            Text("Loading...")
+                                .padding()
+                        )
+                    }
+            }
     }
 }
 
