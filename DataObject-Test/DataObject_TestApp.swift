0b5778108e86cabab65d684d88d0d828b1992c8f
Zach Eriksen <zmeriksen@icloud.com>
2021-03-15 21:09:12 -0500

    Initial Commit

---
+++ DataObject-Test/DataObject_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataObject_TestApp.swift
+//  DataObject-Test
+//
+//  Created by Leif on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DataObject_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
